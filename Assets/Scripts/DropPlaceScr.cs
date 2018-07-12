597c68086c3ce13a88998887431090e6af0f4051
BMK\asu <asu@bmk.by>
2018-07-12 16:39:13 +0300

    Created Canvas, added Cards,tempCard. Started work with position checking

---
+++ Assets/Scripts/DropPlaceScr.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropPlaceScr : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        CardScr card = eventData.pointerDrag.GetComponent<CardScr>();
+        if (card) card.currentParent = transform;
+    }
+}
